95cf437cb2cbdc1c0e96ed44018dd435920f751a
Karel <Karel4ever@gmail.com>
2020-09-15 18:52:51 +0200

    Display Content

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_instagram_stories/models/story_model.dart';
+import 'package:video_player/video_player.dart';
+
+import 'data.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Instagram Stories',
@@ -15,7 +21,66 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Scaffold(),
+      home: StoryScreen(stories: stories),
+    );
+  }
+}
+
+class StoryScreen extends StatefulWidget {
+  final List<Story> stories;
+
+  const StoryScreen({@required this.stories});
+
+  @override
+  _StoryScreenState createState() => _StoryScreenState();
+}
+
+class _StoryScreenState extends State<StoryScreen> {
+  PageController _pageController;
+  VideoPlayerController _videoController;
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+
+    _videoController = VideoPlayerController.network(widget.stories[2].url)
+      ..initialize().then((value) => setState(() {}));
+    _videoController.play();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: PageView.builder(
+        controller: _pageController,
+        itemCount: widget.stories.length,
+        itemBuilder: (context, i) {
+          final Story story = widget.stories[i];
+          switch (story.media) {
+            case MediaType.image:
+              return CachedNetworkImage(
+                imageUrl: story.url,
+                fit: BoxFit.cover,
+              );
+            case MediaType.video:
+              if (_videoController != null &&
+                  _videoController.value.initialized) {
+                return FittedBox(
+                  fit: BoxFit.cover,
+                  child: SizedBox(
+                    width: _videoController.value.size.width,
+                    height: _videoController.value.size.width,
+                    child: VideoPlayer(_videoController),
+                  ),
+                );
+              }
+          }
+          return const SizedBox.shrink();
+        },
+      ),
     );
   }
 }
